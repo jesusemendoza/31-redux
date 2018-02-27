3d9174a00eb2526a3a2a2e3d9892c7909929c3dc
sundownspecial <je.mendoza@live.com>
2018-02-26 17:23:01 -0800

    set-up dev environment

---
+++ lab-jesus/src/components/dashboard/dashboard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {categoryCreate} from '../../actions/category-actions'
+import CategoryForm from '../category/category-form/category-form'
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <section>
+        <h1>Welcome to my Kanban Board</h1>
+
+        <CategoryForm
+          buttonText='create'
+          onComplete={this.props.dashboardCategoryCreate}/>
+
+        {this.props.categories ?
+          this.props.categories.map(cat =>
+            <div key={cat._id} onDoubleClick={() => this.setState({edit: !cat.edit})}>
+              <h3>{cat.name}</h3>
+              <a>{cat.budget}</a>
+              {console.log(cat.edit)}
+            </div>)
+          :
+          undefined
+        }
+      </section>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  categories: state
+})
+
+const mapDispatchToProps = (dispatch, getState) => ({
+  dashboardCategoryCreate: category => dispatch(categoryCreate(category)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
